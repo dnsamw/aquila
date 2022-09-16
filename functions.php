35f146d229f84b8931f3346578afc7e70caeafeb
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2022-09-17 00:29:26 +0530

    enqueue scripts

--- functions.php
+++ functions.php
@@ -8,12 +8,25 @@
 
 function aquila_enqueue_scripts()
 {
-    wp_enqueue_style(
-        'stylesheet', //handle
+    //register styles
+    wp_register_style(
+        'style-css', //handle
         get_stylesheet_uri(), //stylesheet path
         [], //dependency array
         filemtime(get_template_directory() . '/style.css'), //dynamic version number based on modify time
         'all' // screen sizes
     );
+
+    //register script
+    wp_register_script(
+        'main-js', //handle
+        get_template_directory_uri() . '/assets/js/main.js', //path
+        filemtime(get_template_directory() . '/assets/js/main.js'), //dynamic ver number
+        true // include in footer
+    );
+
+    //enqueue script and css
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('main-js');
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
